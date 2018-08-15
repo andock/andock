0894d9fc12103a8dfc0a17b26be1a40b3a69dbf2
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-08-16 00:34:39 +0200

    Update andock installation routine.

--- bin/andock.sh
+++ bin/andock.sh
@@ -5,7 +5,7 @@ ANDOCK_VERSION=0.0.2
 
 REQUIREMENTS_ANDOCK_BUILD='0.0.8'
 REQUIREMENTS_ANDOCK_FIN='0.0.7'
-REQUIREMENTS_ANDOCK_SERVER='0.0.1'
+REQUIREMENTS_ANDOCK_SERVER='0.0.2'
 REQUIREMENTS_SSH_KEYS='0.3'
 
 DEFAULT_CONNECTION_NAME="default"
@@ -789,7 +789,13 @@ run_server_ssh_add ()
     local connection=$1
     shift
 
-    local ssh_key="command=\"andock-server _bridge \$SSH_ORIGINAL_COMMAND\" $1"
+    if [ "$1" = "" ]; then
+        local key=$(cat ~/.ssh/id_rsa.pub)
+    else
+        local key=$1
+    fi
+
+    local ssh_key="command=\"andock-server _bridge \$SSH_ORIGINAL_COMMAND\" $key"
     shift
 
     if [ "$1" = "" ]; then
@@ -832,13 +838,14 @@ run_server_install ()
 
     ansible andock-docksal-server -e "ansible_ssh_user=$root_user" -i "${ANDOCK_INVENTORY}/${connection}"  -m raw -a "test -e /usr/bin/python || (apt -y update && apt install -y python-minimal)"
     ansible-playbook -e "ansible_ssh_user=$root_user" --tags $tag -i "${ANDOCK_INVENTORY}/${connection}" -e "pw='$andock_pw_enc'" "${ANDOCK_PLAYBOOK}/server_install.yml"
+    run_server_ssh_add "$connection"
+    ansible andock-docksal-server -e "ansible_ssh_user=andock" -i "${ANDOCK_INVENTORY}/${connection}"  -m raw -a "andock-server install" -b -e "ansible_sudo_pass=${andock_pw}" -vvv
     if [ "$tag" == "install" ]; then
+        echo-green "andock password is: ${andock_pw}"
         echo-green "andock server was installed successfully."
-        echo-green "andock password is: $andock_pw"
     else
         echo-green "andock server was updated successfully."
     fi
-
 }
 
 #----------------------------------- MAIN -------------------------------------
