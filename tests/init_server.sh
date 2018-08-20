b06041791c9791b004b75fcd338077699440e663
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-08-20 21:51:03 +0200

    Reset server setup.

--- tests/init_server.sh
+++ tests/init_server.sh
@@ -7,19 +7,17 @@ set -e
 slug=$1
 
 # Set default to "ubuntu-18-04-x64"
-if [ "${slug}" = "" ]; then
-    slug="ubuntu-16-04-x64"
+if [ "${slug}" == "" ]; then
+    slug="ubuntu-18-04-x64"
 fi
 
-# Check if a droplet exists and if found delete it.
-echo "Search for old droplet with name andock-travis"
-old_doctl_id=$(doctl compute droplet list 'andock-travis' --no-header --format=ID)
-if [ "${old_doctl_id}" != "" ]; then
-    echo "Old droplet found."
-    echo "Restore droplet with id: ${old_doctl_id}..."
-    doctl compute droplet-action restore ${old_doctl_id} --image-id 37399306 --wait
-    echo "Restore done!"
-    echo "Sleep for a minute and relaxe."
-    sleep 60
-fi
-
+# Create new droplet.
+echo "Create new droplet with image id: ${slug}"
+doctl_id=$(doctl compute droplet create andock-travis --size 1gb --image ${slug} --region fra1 --ssh-keys b6:3c:4d:07:15:8c:7c:43:43:71:e3:61:a3:2d:f1:db,4b:df:d0:74:35:11:97:a7:93:87:a7:4a:b1:66:9b:a3 --wait --no-header --format ID)
+echo "Droplet was created successfully. ID: ${doctl_id}"
+doctl_ip=$(doctl compute droplet list 'andock-travis-${slug}' --no-header --format='Public IPv4')
+# Assign floating ip.
+sudo echo "dev.andock.ci ${doctl_ip}" >> /etc/hosts
+cat /etc/hosts
+echo "YUUUPP! BAAAM!. Sleep for 5 minutes to be sure everything is up."
+sleep 360
---
+++ tests/remove_server.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+# Script creates a new test droplet and
+# assigns an floating ip to the droplet
+
+set -e
+
+slug=$1
+
+# Set default to "ubuntu-18-04-x64"
+if [ "${slug}" == "" ]; then
+    slug="ubuntu-18-04-x64"
+fi
+
+# Check if a droplet exists and if found delete it.
+echo "Search for old droplet with name andock-travis-${slug}"
+old_doctl_id=$(doctl compute droplet list 'andock-travis-${slug}' --no-header --format=ID)
+if [ "${old_doctl_id}" != "" ]; then
+    echo "Old droplet found."
+    echo "Delete droplet with id: ${old_doctl_id}..."
+    doctl compute droplet delete andock-travis -f
+    echo "Deleting done."
+fi
