76319583cbb9d1d8af5c7c77b03986b287592122
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-08-28 11:28:04 +0200

    Fix docker installtion.

--- tests/lxd/install.sh
+++ tests/lxd/install.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env sh
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo docker ps
 sudo apt-get -qq update;
-sudo apt-get -y install cgroup-lite
-sudo apt-get -y install docker-ce
 sudo apt-get -y install snapd;
 sudo snap install lxd;
 sudo sh -c 'echo PATH=/snap/bin:$PATH >> /etc/environment';
 while [ ! -S /var/snap/lxd/common/lxd/unix.socket ]; do echo "Waiting for LXD socket...";sleep 0.2;done;
 sudo lxd init --auto;
 sudo usermod -a -G lxd travis;
-
-
 #sudo su travis -c 'lxc network create lxdbr0';
 #sudo su travis -c 'lxc network attach-profile lxdbr0 default eth0';
