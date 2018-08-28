461a01d81b3f1a5d02743df481ed88675b99613d
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-08-28 22:35:14 +0200

    Fix tests

--- tests/lxd/install.sh
+++ tests/lxd/install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env sh
-sudo docker ps
+# exit on fail
+set -eo pipefail
+# setup LXD group
+sudo groupadd --system lxd
+sudo usermod -a -G lxd $USER
 sudo apt-get -qq update;
 sudo apt-get -y install snapd;
 sudo snap install lxd;
 sudo sh -c 'echo PATH=/snap/bin:$PATH >> /etc/environment';
-while [ ! -S /var/snap/lxd/common/lxd/unix.socket ]; do echo "Waiting for LXD socket...";sleep 0.2;done;
-sudo lxd init --auto;
-sudo lxc --version;
-sudo usermod -a -G lxd travis;
-sudo su travis -c 'lxc network create lxdbr0';
+
+sudo lxd waitready
+printf 'n\ny\ndefault\ndir\nn\ny\nlxdbr0\nauto\nauto\ny\nall\n\ny\nn\n' | sudo lxd init
+sudo lxd.lxc profile set travis security.privileged true
+sudo lxd.lxc profile copy default travis
+
+#sudo su travis -c 'lxc network create lxdbr0';
 #sudo su travis -c 'lxc network attach-profile lxdbr0 default eth0';
