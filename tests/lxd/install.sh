d997750d0d2ef6f47590c29b3f24932ec3ccbf68
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-08-28 12:57:44 +0200

    Add network

--- tests/lxd/install.sh
+++ tests/lxd/install.sh
@@ -7,5 +7,5 @@ sudo sh -c 'echo PATH=/snap/bin:$PATH >> /etc/environment';
 while [ ! -S /var/snap/lxd/common/lxd/unix.socket ]; do echo "Waiting for LXD socket...";sleep 0.2;done;
 sudo lxd init --auto;
 sudo usermod -a -G lxd travis;
-#sudo su travis -c 'lxc network create lxdbr0';
-#sudo su travis -c 'lxc network attach-profile lxdbr0 default eth0';
+sudo su travis -c 'lxc network create lxdbr0';
+sudo su travis -c 'lxc network attach-profile lxdbr0 default eth0';
