e59d374a1d5c5fa5106076b1e8864f19bb814eaf
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-08-28 09:30:19 +0200

    Install docksal by hand

--- tests/digitalocean/init_ssh.sh
+++ tests/digitalocean/init_ssh.sh
@@ -5,7 +5,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in tests/digital-ocean/andock_key.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in andock_key.enc -out ~/.ssh/id_rsa -d
 cp id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
