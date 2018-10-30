eeb6087aa4f3faee9d8b560556ea21147a663d6e
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-10-30 08:38:59 +0100

    Remove opensserver from init_travis.

--- tests/travis/install.sh
+++ tests/travis/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-sudo apt-get install openssh-server -y
-sudo /etc/init.d/ssh start
+#sudo apt-get install openssh-server -y
+#sudo /etc/init.d/ssh start
 
 
 #sudo apt-get install acl -y
